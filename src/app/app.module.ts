df4e4e9048b4914331797881508622e10d2e8102
Alexandru Turcitu <turcitua@gmail.com>
2017-06-13 02:44:22 +0300

    slicing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
+// IMPORTS CORE
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { provideRoutes} from '@angular/router';
+import { AppComponent } from './app.component';
+import { routes } from './app.routes';
+import {FormsModule} from "@angular/forms";
+import * as $ from 'jquery';
+import { Angular2FontAwesomeModule } from 'angular2-font-awesome/angular2-font-awesome';
+
+
+// IMPORTS NGX-BOOSTRAP COMPONENTS
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { AccordionModule } from 'ngx-bootstrap/accordion';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
-import { AppComponent } from './app.component';
-import { Angular2FontAwesomeModule } from 'angular2-font-awesome/angular2-font-awesome';
-import * as $ from 'jquery';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { AlertModule } from 'ngx-bootstrap/alert';  
+
+// IMPORTS PAGES
+import { ElementsComponent } from './pages/elements/elements.component';
 import { AboutComponent } from './about/about.component';
 import { ServicesComponent } from './services/services.component';
-import { routes } from './app.routes';
-import { RouterModule } from '@angular/router';
-import { provideRoutes} from '@angular/router';
-import { ElementsComponent } from './pages/elements/elements.component';
-import {FormsModule} from "@angular/forms";
+
 
 @NgModule({
   declarations: [
@@ -26,6 +35,8 @@ import {FormsModule} from "@angular/forms";
     BsDropdownModule.forRoot(),
     AccordionModule.forRoot(),
     PaginationModule.forRoot(),
+    TabsModule.forRoot(),
+    AlertModule.forRoot(),
     Angular2FontAwesomeModule,
     FormsModule,
     //RouterModule.forRoot(routes),
