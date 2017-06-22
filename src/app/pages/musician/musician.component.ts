148a36572a7e020e7519f176bef702f499a2c05c
Alexandru Turcitu <turcitua@gmail.com>
2017-06-22 18:15:01 +0300

    all pages

--- src/app/pages/musician/musician.component.ts
+++ src/app/pages/musician/musician.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MusicianComponent implements OnInit {
 
+  public totalItems: number = 64;
+  public currentPage: number = 4;
+  public smallnumPages: number = 0;
+ 
+  public setPage(pageNo: number): void {
+    this.currentPage = pageNo;
+  }
+ 
+  public pageChanged(event: any): void {
+    console.log('Page changed to: ' + event.page);
+    console.log('Number items per page: ' + event.itemsPerPage);
+  }
+
+
+
   constructor() { }
 
   ngOnInit() {
